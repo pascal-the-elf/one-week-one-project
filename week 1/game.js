180d8677670848556fb1e6c8b61e782ec014cb2d
JacobLinCool <jacoblin2011@gmail.com>
2019-05-22 20:29:41 +0800

    Update game.js

--- week 1/game.js
+++ week 1/game.js
@@ -4,7 +4,10 @@ var Game = {
     },
     "draw": {
         "start": function() {
-            
+            ctx.fillRect(0, 0, 224, 288);
+            ctx.fillText("High", 10, 50);
+            ctx.fillText("Score", 10, 150);
+            ctx.fillText("Life", 10, 200);
         }
     },
     "data": {
