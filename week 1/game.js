925479627260500640c548928f7af87a85d92f51
JacobLinCool <jacoblin2011@gmail.com>
2019-05-24 11:01:46 +0800

    Update game.js

--- week 1/game.js
+++ week 1/game.js
@@ -1,21 +1,29 @@
 var Game = {
     "init": function() {
         ctx.font = "200px VT323";
+        this.data.high = localStorage.PacManHigh ? localStorage.PacManHigh : 0;
         this.draw.start();
     },
     "draw": {
         "start": function() {
+            this.update();
+        },
+        "update" : function() {
             ctx.fillStyle = "#000";
             ctx.fillRect(0, 0, 2240, 2880);
             ctx.fillStyle = "#fff";
-            ctx.fillText("High", 550, 120);
-            ctx.fillText("Score", 1300, 120);
-            ctx.fillText("Life", 1800, 120);
+            ctx.fillText("High", 50, 120);
+            ctx.fillText("Score", 800, 120);
+            ctx.fillText("Life", 1550, 120);
+            ctx.fillText(Game.data.high, 50, 120*2);
+            ctx.fillText(Game.data.score, 800, 120*2);
+            ctx.fillText(Game.data.life, 1550, 120*2);
         }
     },
     "data": {
         "stage": 0,
         "score": 0,
-        "life": 3
+        "life": 3,
+        "high": 0
     }
 };
