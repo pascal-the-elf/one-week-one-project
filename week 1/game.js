5bcb8873af9fa027784f91a9b3a4deff51b46641
JacobLinCool <jacoblin2011@gmail.com>
2019-05-25 13:39:41 +0800

    Update game.js

--- week 1/game.js
+++ week 1/game.js
@@ -1,23 +1,47 @@
+// 19 * 22 (19*100, 22*100)
+// 27.5 / 19*115 / 27.5
 var Game = {
     "init": function() {
+        ctx.font = "160px Arial";
+        ctx.fillText("Loading...", 10, 160);
         ctx.font = "200px VT323";
         this.data.high = localStorage.PacManHigh ? localStorage.PacManHigh : 0;
-        this.draw.start();
+        setTimeout(function() { Game.draw.start(); }, 1000);
     },
     "draw": {
         "start": function() {
-            this.update();
-        },
-        "update" : function() {
             ctx.fillStyle = "#000";
             ctx.fillRect(0, 0, 2240, 2880);
             ctx.fillStyle = "#fff";
             ctx.fillText("High", 50, 120);
             ctx.fillText("Score", 800, 120);
             ctx.fillText("Life", 1550, 120);
+            this.update();
+            this.map();
+        },
+        "update" : function() {
+            ctx.fillStyle = "#fff";
             ctx.fillText(Game.data.high, 50, 120*2);
             ctx.fillText(Game.data.score, 800, 120*2);
             ctx.fillText(Game.data.life, 1550, 120*2);
+        },
+        "map": function() {
+            ctx.beginPath();
+            ctx.moveTo(27.5, 300);
+            ctx.lineTo(27.5 + 19 * 115, 300);
+            ctx.lineTo(27.5 + 19 * 115, 300 + 7 * 115);
+            ctx.lineTo(27.5 + 16 * 115, 300 + 7 * 115);
+            ctx.lineTo(27.5 + 16 * 115, 300 + 9 * 115);
+            ctx.lineTo(27.5 + 20 * 115, 300 + 9 * 115);
+            ctx.lineTo(27.5 + 20 * 115, 300 + 11 * 115);
+            ctx.lineTo(27.5 + 16 * 115, 300 + 11 * 115);
+            ctx.lineTo(27.5 + 16 * 115, 300 + 13 * 115);
+            ctx.lineTo(27.5 + 19 * 115, 300 + 13 * 115);
+            ctx.lineTo(27.5 + 19 * 115, 300 + 21 * 115);
+            ctx.lineTo(27.5, 300 + 21 * 115);
+            ctx.strokeStyle = "blue";
+            ctx.lineWidth = 10;
+            ctx.stroke();
         }
     },
     "data": {
