2adf529fbd74d752c404088a9c0036a04396558f
JacobLinCool <jacoblin2011@gmail.com>
2019-05-25 18:30:25 +0800

    Update game.js

--- week 1/game.js
+++ week 1/game.js
@@ -1,11 +1,9 @@
-// 17 * 20 (17*120, 20*120)
-// 100 / 17*120 / 100
 // dot-space: 27 * 30
 var Game = {
     "init": function() {
         ctx.font = "160px Arial";
         ctx.fillText("Loading...", 10, 160);
-        ctx.font = "200px VT323";
+        ctx.font = "160px VT323";
         this.data.high = localStorage.PacManHigh ? localStorage.PacManHigh : 0;
         setTimeout(function() { Game.draw.start(); }, 1000);
     },
@@ -14,21 +12,21 @@ var Game = {
             ctx.fillStyle = "#000";
             ctx.fillRect(0, 0, 2240, 2880);
             ctx.fillStyle = "#fff";
-            ctx.fillText("High", 50, 120);
-            ctx.fillText("Score", 800, 120);
-            ctx.fillText("Life", 1550, 120);
+            ctx.fillText("High", 50, 100);
+            ctx.fillText("Score", 800, 100);
+            ctx.fillText("Life", 1550, 100);
             this.update();
             this.map();
         },
         "update" : function() {
             ctx.fillStyle = "#fff";
-            ctx.fillText(Game.data.high, 50, 120*2);
-            ctx.fillText(Game.data.score, 800, 120*2);
-            ctx.fillText(Game.data.life, 1550, 120*2);
+            ctx.fillText(Game.data.high, 50, 100*2);
+            ctx.fillText(Game.data.score, 800, 100*2);
+            ctx.fillText(Game.data.life, 1550, 100*2);
         },
         "map": function() {
             var ds = 2200 / 27;
-            var pd = 20, hpd = 300;
+            var pd = 20, hpd = 220;
             ctx.lineCap = "round";
             // Border
             ctx.beginPath();
